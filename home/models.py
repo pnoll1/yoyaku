c717b20824d259b92397774b5f56d22ed04377b7
pnoll1 <8444617+pnoll1@users.noreply.github.com>
2019-06-08 17:18:14 -0700

    added path tests, cleaned up test models, fixed search url

--- home/models.py
+++ home/models.py
@@ -5,36 +5,25 @@ class place(models.Model):
     name_en = models.TextField(blank=True)
     city = models.TextField()
     cuisine  = models.TextField(blank=True)
-    hours = models.TextField(blank=True)
-    english_friendly = models.BooleanField(blank=True)
-    outdoor_seating = models.BooleanField(blank=True)
-    website = models.TextField(blank=True)
-    # link to menu in static folder
-    menu = models.TextField(blank=True)
-    lon = models.FloatField()
-    lat = models.FloatField()
-
-    def __str__(self):
-        return self.name
-# planet_osm_point table for tags osm_id first Col
-# planet_osm_nodes table with id, lat ,long
-
-class planet_osm_point(models.Model):
-    name = models.TextField()
-    name_en = models.TextField(blank=True)
-    cuisine  = models.TextField(blank=True)
     opening_hours = models.TextField(blank=True)
     #english_friendly = models.BooleanField(blank=True)
     outdoor_seating = models.BooleanField(blank=True)
     website = models.TextField(blank=True)
+    # link to menu in static folder
+    #menu = models.TextField(blank=True)
     #phone = models.TextField(blank=True)
     # uuid =models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     #osm_id
     city = models.TextField()
     way = models.PointField()
-    geom = models.PointField()
+
     def __str__(self):
         return self.name
+
+    class Meta:
+        managed = False
+        db_table = 'home_planet_osm_point'
+
 
 class reservation(models.Model):
     requested_by_user = models.TextField()
--- home/urls.py
+++ home/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('search', views.search),
+    path('?search', views.search),
     path('login', views.login),
     path('register', views.register),
 ]
